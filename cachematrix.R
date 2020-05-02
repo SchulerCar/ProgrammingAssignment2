91aecec1877f4666feaec89cee03c1ff756c39e0
SchulerCar <SchulerCar@yahoo.com>
2020-05-01 20:31:53 -0700

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions
 ##
+## makeCacheMatrix <- function(nm= matrix())
+##    Creates a special "matrix" object with the following methods
+##      setMatrix(nm):  sets the value of the matrix to nm
+##      getMatrix():    gets the value of the matrix
+##      setInverse(mi): sets the value of the matrix inverse to mi
+##      getInverse():   gets the value of the matrix inverse
+##
+##
+## cachesSolve(specialMatrix)
+##    Calculates the inverse of the special "matrix" created with the above
+##    function. 
+##     - However, it first checks to see if the inverse has already been 
+##       calculated. If so, it gets the inverse from the cache and skips the 
+##       computation. 
+##     - Otherwise, it calculates the inverse of the data and sets the value of the 
+##       mean in the cache via the setmean function.
 
-## Write a short comment describing this function
+
+## This is the special "matrix" constructor
 
 makeCacheMatrix <- function(x = matrix()) {
         ## Initialize local inverse variable
@@ -29,7 +45,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This is the function that calculates the inverse for the special "matrix"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -39,7 +55,7 @@ cacheSolve <- function(x, ...) {
                 return(matrixInverse)
         }
         theMatrix <- x$getMatrix()
-        matrixInverse <- solve(theMatrix)
+        matrixInverse <- solve(theMatrix, ...)
         x$setInverse (matrixInverse)
         matrixInverse
 }
